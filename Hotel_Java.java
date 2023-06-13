c1ed1d1ca4fee13f2cd9a54ee08125dc203c2333
David Altamira <davidaltamira19@gmail.com>
2023-06-12 23:34:55 -0600

    Hotel en Java

---
+++ Hotel_Java.java
@@ -0,0 +1,28 @@
+package TiposDeDatosYVariables;
+
+public class Hotel_Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte pisos = 16;
+		System.out.println(pisos);
+		
+		short cuartos = 1600;
+		System.out.println(cuartos);
+		
+		int huespedes = 6400;
+		System.out.println(huespedes);
+		
+		long ingresomensual = 2000000;
+		System.out.println(ingresomensual);
+		
+		float precioporhabitacion = 120.99f;
+		System.out.println(precioporhabitacion);
+		
+		boolean allinclusive = true;
+		System.out.println(allinclusive);		
+		
+	}
+
+}
